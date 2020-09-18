5dc2f46be7af9408087b0c6a9b2c224130f7e306
Tania Sainvilus <taniasainvilus@Tanias-Air.hsd1.ga.comcast.net>
2020-09-18 12:54:56 -0400

    change

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+const express = require('express'); 
+const app = express();
+app.use(express.static(__dirName + '/public'));
+app.get('/', (req, res) => {
+     res.sendFile('index.html', {root: __dirname + '/public/'});
+});
+app.listen(process.env.PORT || 5000)
+
 
 // Nav 
 const showMenu = (toggleId, navId) =>{
